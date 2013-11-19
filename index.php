57053fa78c5ad5426e779d5b6b73cd10e5fd8597
Brenden <brenden@brenden.me>
2013-11-19 02:49:15 -0600

    Syntax error fix

--- index.php
+++ index.php
@@ -81,7 +81,6 @@ if (!$_POST) {
             
             echo "Message: " . $message . "<br />";
         } else {
-\
             $file = time()."_".$_FILES["file"]["name"];
             $dir = getcwd();
             $session_id = sha1($file);
